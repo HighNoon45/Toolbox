d45e1f0915cb24e65119a59174e23fc1560429ee
akhmedov <akhmedov@DESKTOP-V5M89SE>
2025-06-10 19:31:05 +0200

    - LoginView (usercontrol) implementiert - bindings, buttons, logik folgt

---
+++ Chatbot.GUI/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Chatbot.GUI.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Chatbot.GUI
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var loginWindow = new LoginWindow();
+            loginWindow.Show();
+        }
+    }
+}
